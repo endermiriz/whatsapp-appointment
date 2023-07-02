fb78333fca1a70c25c3ccd124e4fb4cb902f51a4
TeomanM <teomanmdr@protonmail.com>
2023-07-03 01:18:37 +0300

    Add configs for pony and create secret key

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
-
+from pony.flask import Pony
+from pony.orm import Database
 
 app = Flask(__name__)
+db = Database()
+pony = Pony()
 
+def register_extensions(app):
+    pass
 
 def create_app(config):
     app.config.from_object(config)
--- apps/config.py
+++ apps/config.py
@@ -1,4 +1,10 @@
 import os
 
+
 class Config(object):
-    pass
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    PONY = {
+        "provider": os.getenv("PROVIDER", "sqlite"),
+        "filename": os.getenv("DB_FILENAME", "project.db"),
+        "create_db": bool(os.getenv("CREATE_DB", True)),
+    }
